73e991051dc5fba4a83f48a4fae5dbbffbe792e4
lucasf_ <lfeliualvarez@danielcastelao.org>
2022-12-13 10:33:04 +0100

    Mensaje de confirmación

--- script_git.sh
+++
@@ -1,8 +0,0 @@
-# Agregar todos los cambios en la carpeta al repositorio local
-git add .
-
-# Confirmar cambios y agregar un mensaje de confirmación
-git commit -m "Mensaje de confirmación"
-
-# Subir cambios al repositorio en GitHub
-git push origin master
